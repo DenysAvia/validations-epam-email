b25a854009c3f64d23fd828395e35292d2a88f07
Denys Kovika <d.v.kovika@gmail.com>
2022-08-15 13:49:09 +0300

    ver.1

--- src/main/java/com/epam/rd/autotasks/validations/EpamEmailValidation.java
+++ src/main/java/com/epam/rd/autotasks/validations/EpamEmailValidation.java
@@ -7,7 +7,14 @@ public class EpamEmailValidation {
 
     public static boolean validateEpamEmail(String email) {
         // Put your code here
+        String regexpr = "^[a-zA-Z\\d_!#$%&'*+/=?`{|}~^.-]+_[a-zA-Z\\d_!#%&'*+/=?`{|}~^.-]+@[a-zA-Z\\d.-]+.[com]+$";
+        if(email == null || email.equals("") || email.equals(" ")) return false;
+        Pattern p = Pattern.compile(regexpr);
 
+        Matcher matcher = p.matcher(email);
+
+
+        return matcher.matches();
     }
 }
 
